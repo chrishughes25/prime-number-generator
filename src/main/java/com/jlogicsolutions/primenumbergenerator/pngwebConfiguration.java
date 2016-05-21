d45eba26ef5513fc6e7c7c87dbba2b0154b11097
chris <chris.hughes25@gmail.com>
2016-05-21 17:32:31 +0100

    initial commit

---
+++ src/main/java/com/jlogicsolutions/primenumbergenerator/pngwebConfiguration.java
@@ -0,0 +1,10 @@
+package com.jlogicsolutions.primenumbergenerator;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class pngwebConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
